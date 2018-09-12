9a1ac51ade17575f25331725952ff7de29922b45
Will Figueiredo <will@wfigueiredo.com>
2018-09-12 03:24:57 +0000

    automatically backed up by learn

--- spec/times_spec.rb
+++ spec/times_spec.rb
@@ -1,10 +1,16 @@
 require_relative "spec_helper"
 require_relative "../times.rb"
 
-describe "#using_times" do 
+describe "#using_times" do
 	it "puts out the phrase 'Wingardium Leviosa' 7 times by using the `times` construct" do
 
 	looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
-	expect{ using_times }.to output(looping_string).to_stdout 
+	expect{ using_times }.to output(looping_string).to_stdout
+	end
+end
+
+def using_times
+	7.times do
+		puts "Wingardium Leviosa"
 	end
 end
